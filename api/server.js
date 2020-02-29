f7cea86285210fa928d8b0e90de38ff4f7b5983b
skye <skyesajous@gmail.com>
2020-02-29 13:39:00 -0700

    sign up, login, authentication

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express();
+
+// routers
+const authRouter = require("../routers/auth/authRouter");
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  return res.send("Welcome to the API");
+});
+
+module.exports = server;
